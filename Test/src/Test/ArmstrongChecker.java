1b9a0a3c3230dd1ce115360cd7a8b40e1ab3deb3
CloudJumper08 <raturi.devashish617@gmail.com>
2025-08-11 22:36:32 +0530

    Armstrong number check

---
+++ Test/src/Test/ArmstrongChecker.java
@@ -0,0 +1,29 @@
+package Test;
+import java.util.Scanner;
+
+public class ArmstrongChecker {
+    // Method to check if a number is an Armstrong number
+    public static boolean isArmstrong(int num) {
+        int original = num;
+        int sum = 0;
+        int digits = String.valueOf(num).length();
+        while (num > 0) {
+            int digit = num % 10;
+            sum += Math.pow(digit, digits);
+            num /= 10;
+        }
+        return sum == original;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        if (isArmstrong(num)) {
+            System.out.println(num + " is an Armstrong number.");
+        } else {
+            System.out.println(num + " is not an Armstrong number.");
+        }
+        scanner.close();
+    }
+}
