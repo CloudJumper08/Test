5166c06a5683d5cd5d12527e9e8b499d3e3c5ff6
CloudJumper08 <raturi.devashish617@gmail.com>
2025-08-11 22:33:00 +0530

    Program to reverse a string without using StringBuilder.reverse()

--- Test/src/Test/PalindromeChecker.java
+++ Test/src/Test/PalindromeChecker.java
@@ -15,10 +15,25 @@ public class PalindromeChecker {
         return true;
     }
 
+    public static String reverseString(String str) {
+        char[] chars = str.toCharArray();
+        int left = 0, right = chars.length - 1;
+        while (left < right) {
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
+            left++;
+            right--;
+        }
+        return new String(chars);
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a string: ");
         String input = scanner.nextLine();
+        String reversed = reverseString(input);
+        System.out.println("Reversed string: " + reversed);
         if (isPalindrome(input)) {
             System.out.println("The string is a palindrome.");
         } else {
