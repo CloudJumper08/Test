dcd97ff6898ffc2c399fc4a3df0b72a115c74f9c
CloudJumper08 <raturi.devashish617@gmail.com>
2025-08-11 17:57:05 +0530

    First Commit

--- Test/src/Test/HelloWorld.java
+++ Test/src/Test/HelloWorld.java
@@ -5,6 +5,14 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, World!");
+		System.out.println("This is a simple Java program.");
+		System.out.println("It prints messages to the console.");
+		System.out.println("Feel free to modify it as you wish.");
+		System.out.println("Happy coding!");
+		System.out.println("This is a test for the Java program.");
+		System.out.println("Let's add some more lines to test the output.");
+		System.out.println("Testing complete. Goodbye!");
+			
 	}
 
 }
